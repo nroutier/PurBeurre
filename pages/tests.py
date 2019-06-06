750b084ec4816bfb21507a7cc77f365d03ea45df
unknown <nroutier@gmail.com>
2019-06-06 18:34:15 +0200

    Add test to pages app

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class HomePageTests(SimpleTestCase):
+
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+class LegalPageTests(SimpleTestCase):
+
+
+    def test_legal_page_status_code(self):
+        response = self.client.get('/legal/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('legal'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('legal'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'legal.html')
